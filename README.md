29ef503e4e91fa658afc4051eea805b9bbfd1b0d
LuisMayo <luismayovalbuena@outlook.es>
2021-06-23 16:57:35 +0200

    Added inline mode

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ set TOKEN=your-bot-token && deno run --allow-net --allow-env main.ts
 
 ## Contributing
 Since this is a tiny project we don't have strict rules about contributions. Just open a Pull Request to fix any of the project issues or any improvement you have percieved on your own. Any contributions which improve or fix the project will be accepted as long as they don't deviate too much from the project objectives. If you have doubts about whether the PR would be accepted or not you can open an issue before coding to ask for my opinion
+
+## Thanks
+This bot is inspired by [Bweaking NuwuS](https://twitter.com/BBCBweaking) user on Twitter
--- main.ts
+++ main.ts
@@ -13,10 +13,26 @@ const bot = new TelegramBot(TOKEN);
 
 bot.on(UpdateType.Message, async ({ message }) => {
   if (message.text && message.text.startsWith("/uwufy")) {
-    const text = message.reply_to_message?.text || message.text.substring(6) || "I can't hear you";
-    await bot.sendMessage({
+    const text = message.reply_to_message?.text || message.text.substring(6) ||
+      "I can't hear you";
+    bot.sendMessage({
       chat_id: message.chat.id,
       text: uwuifier.uwuifySentence(text),
+    });
+  }
+});
+
+bot.on(UpdateType.InlineQuery, async (query) => {
+  const send = uwuifier.uwuifySentence(query.inline_query.query);
+  if (send && send.length > 0) {
+    await bot.answerInlineQuery({
+      inline_query_id: query.inline_query.id,
+      results: [{
+        type: "article",
+        id: "1",
+        title: send,
+        input_message_content: { message_text: send },
+      }],
     });
   }
 });
